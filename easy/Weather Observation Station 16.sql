fda2949880bdbf01cae934d2c8e4a379b4a50a4b
Dhananjay <dhananjay.pandey72@gmail.com>
2025-11-27 21:25:06 +0530

    Weather Observation Station 16

---
+++ easy/Weather Observation Station 16.sql
@@ -0,0 +1,13 @@
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+*/
+
+SELECT ROUND(MIN(LAT_N),4)
+FROM STATION
+WHERE LAT_N>38.7780;
+
+
+/*
+Expected output
+38.8526
+*/
