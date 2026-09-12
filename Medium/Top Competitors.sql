872d8f7ce9fcf1854e6bcb2f01b0825bc3e569f5
Dhananjay <dhananjay.pandey72@gmail.com>
2026-09-12 22:15:21 +0530

    Top Competitors

---
+++ Medium/Top Competitors.sql
@@ -0,0 +1,11 @@
+--  Top Competitors
+
+SELECT h.hacker_id, h.name
+FROM submissions s
+JOIN challenges c ON s.challenge_id = c.challenge_id
+JOIN difficulty d ON c.difficulty_level = d.difficulty_level
+JOIN hackers h ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(s.challenge_id) > 1
+ORDER BY COUNT(s.challenge_id) DESC, h.hacker_id ASC;
