50b3ef12dc1b698639d58325124b5f1de79f3c60
Dhananjay <dhananjay.pandey72@gmail.com>
2026-09-13 22:31:58 +0530

    Ollivander's Inventory

---
+++ Medium/Ollivander's Inventory.sql
@@ -0,0 +1,12 @@
+--  Ollivander's Inventory
+
+SELECT w.id, p.age, w.coins_needed, w.power
+FROM Wands AS w
+JOIN Wands_Property AS p ON w.code = p.code
+WHERE w.coins_needed = (
+    SELECT MIN(coins_needed)
+    FROM Wands w2
+    INNER JOIN Wands_Property p2 ON w2.code = p2.code
+    WHERE p2.is_evil = 0 AND p.age = p2.age AND w.power = w2.power
+)
+ORDER BY w.power DESC, p.age DESC;
